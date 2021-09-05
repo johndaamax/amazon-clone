270dbb1cf02e80f469d6a93c525a09ccb5132c2f
johndaamax <ia.uoistud@gmail.com>
2021-09-05 14:37:55 +0200

    first commit, cleanup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <h1>Welcome to Amazon Clone. WIP</h1>
+        </Route>
+        <Route path='/checkout' exact>
+          <h1>Checkout Page</h1>
+        </Route>
+        <Route path='/login' exact>
+          <h1>Login Page</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
